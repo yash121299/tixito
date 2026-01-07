9173ec8316d970d7cdeb1761bd3244555cc4e5bc
yash121299 <yash121299@gmail.com>
2026-01-06 18:41:14 -0800

    Initial Spring Boot Project Structure

---
+++ src/main/java/com/yash/tixito/TixitoApplication.java
@@ -0,0 +1,13 @@
+package com.yash.tixito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TixitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TixitoApplication.class, args);
+	}
+
+}
